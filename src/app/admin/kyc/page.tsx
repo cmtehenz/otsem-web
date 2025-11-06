3a8baa6532c5d70390949f8c4a5d467918dfb553
Gustavo Costa <cmtehenz@gmail.com>
2025-11-06 17:51:52 -0300

    ajuste em kyc

--- src/app/admin/kyc/page.tsx
+++ src/app/admin/kyc/page.tsx
@@ -2,16 +2,16 @@
 
 import * as React from "react";
 import Link from "next/link";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import { useDebouncedCallback } from "use-debounce";
 import { toast } from "sonner";
 import { http } from "@/lib/http";
 
 import {
     Loader2,
-    CheckCircle2,
-    XCircle,
-    CircleDot
+    // CheckCircle2,
+    // XCircle,
+    // CircleDot
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
